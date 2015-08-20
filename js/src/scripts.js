67fb4a5966fa9cd669e552647aec7768a7a19892
Michael Treser <armikael2@gmail.com>
2015-08-20 10:51:04 +0300

    Adding elements to the page, styles and their behavior.

--- js/src/scripts.js
+++ js/src/scripts.js
@@ -3,7 +3,112 @@ $(document).ready(function() {
 
 	console.log('JS is up!');
 
-	$('.draggable-item').draggable();
-	$('.draggable-item').selectable();
-	$('.flow-panel').droppable();
+	// Making Elements Draggable
+	$('.draggable-item').draggable({
+		revert: 'invalid',
+		connectTo: '.flow-panel'
+	});
+
+	// Making Dropable Panel
+	$('.flow-panel').droppable({
+		tolerance: "fit"
+	});
+
+
+
+	// Open Modal on Double click
+	$('.draggable-item').on('dblclick', function(e) {
+
+		if (!$(this).next().hasClass('visible')) {
+			$(this).next().fadeIn().toggleClass('visible');
+		} else {
+			$(this).next().fadeOut().toggleClass('visible');
+		}
+	});
+
+
+	// Modal Window Functional Buttons
+	var $selectedValue;
+
+	$('.ok-btn').on('click', function() {
+		console.log($(this).parent().attr('id'))
+
+		if ($(this).parent().attr('id') === 'modalBtns1') {
+			changeBtnStyle();
+
+		} else if ($(this).parent().attr('id') === 'modalBtns2') {
+			changeProgressStyle();
+
+		} else if ($(this).parent().attr('id') === 'modalBtns3') {
+			changeThmbStyle();
+
+		} else if ($(this).parent().attr('id') === 'modalBtns4') {
+			changeIconStyle();
+			
+		} else if ($(this).parent().attr('id') === 'modalBtns5') {
+			changeMediaStyle();
+		}
+
+		$(this).closest('.modal-window').fadeOut().removeClass('visible');
+	});
+
+
+	// Changing button styles
+	var changeBtnStyle = function() {
+		$selectedValue = $('#selectBox1 option:selected').val();
+
+		if (!$('#dragBtn').hasClass($selectedValue)) {
+			$('#dragBtn').removeClass('label-primary label-danger')
+			$('#dragBtn').addClass($selectedValue);
+		}
+	};
+
+
+	// Changing progress bar styles 
+	var changeProgressStyle = function() {
+		$selectedValue = $('#selectBox2 option:selected').val();
+
+		$('#dragProgress').css('width', $selectedValue);
+	}
+
+
+	// Changing thumbnail styles
+	var changeThmbStyle = function() {
+		$selectedValue = $('#selectBox3 option:selected').val();
+
+		if (!$('#dragThumbn').hasClass($selectedValue)) {
+			$('#dragThumbn').removeClass();
+			$('#dragThumbn').addClass($selectedValue);
+		}
+	}
+
+	// Changing glyphicon styles
+	var changeIconStyle = function() {
+		$selectedValue = $('#selectBox4 option:selected').val();
+
+		if (!$('#dragIcon').hasClass($selectedValue)) {
+			$('#dragIcon').removeClass('rotate-left rotate-right');
+			$('#dragIcon').addClass($selectedValue);
+		}
+	}
+
+
+	// Changing media box styles
+	var changeMediaStyle = function() {
+		$selectedValue = $('#selectBox5 option:selected').val();
+
+		if (!$('#dragMedia').hasClass($selectedValue)) {
+			$('#dragMedia').removeClass();
+			$('#dragMedia').addClass($selectedValue);
+		}
+	} 
+
+
+	$('.cancel-btn').on('click', function() {
+		$(this).closest('.modal-window').fadeOut().removeClass('visible');
+	});
+
+
+
+	
 });
